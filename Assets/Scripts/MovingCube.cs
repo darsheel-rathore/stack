b6bafaf316e360abfbfa7ea686f88a372f1ca76b
Darsheel Rathore <darsheel.techdev@gmail.com>
2023-08-10 19:25:15 +0530

    Both Cube Split Finished

--- Assets/Scripts/MovingCube.cs
+++ Assets/Scripts/MovingCube.cs
@@ -25,10 +25,12 @@ public class MovingCube : MonoBehaviour
         moveSpeed = 0f;
 
         float hangover = transform.position.z - LastCube.transform.position.z;
-        SplitCubeOnZ(hangover);
+        float direction = hangover > 0 ? 1f : -1f;
+
+        SplitCubeOnZ(hangover, direction);
     }
 
-    private void SplitCubeOnZ(float hangover)
+    private void SplitCubeOnZ(float hangover, float direction)
     {
         float newZSize = LastCube.transform.localScale.z - Mathf.Abs(hangover);
         float fallingBlockSize = transform.localScale.z - newZSize;
@@ -38,8 +40,8 @@ public class MovingCube : MonoBehaviour
         transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, newZSize);
         transform.position = new Vector3(transform.position.x, transform.position.y, newZPosition);
 
-        float cubeEdge = transform.position.z + (newZSize / 2f);
-        float fallingBlockZPosition = cubeEdge + fallingBlockSize / 2f;
+        float cubeEdge = transform.position.z + (newZSize / 2f * direction);
+        float fallingBlockZPosition = cubeEdge + (fallingBlockSize / 2f * direction);
 
         SpawnDropCube(fallingBlockZPosition, fallingBlockSize);
     }
@@ -47,7 +49,12 @@ public class MovingCube : MonoBehaviour
     private void SpawnDropCube(float fallingBlockZPosition, float fallingBlockSize)
     {
         var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
         cube.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, fallingBlockSize);
+        
         cube.transform.position = new Vector3(transform.position.x, transform.position.y, fallingBlockZPosition);
+
+        cube.AddComponent<Rigidbody>();
+        Destroy(cube.gameObject, 1f);
     }
 }
