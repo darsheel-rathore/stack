15d39e2c62c0a0b70ce982063c30262236509e0c
Darsheel Rathore <darsheel.techdev@gmail.com>
2023-08-10 17:43:56 +0530

    Chopping the Moving cube to the Static Cube (First Cube) Size, not the falling cube

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     private void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1"))
         {
             MovingCube.CurrentCube.Stop();
-            Debug.Log("Game Manager update called.");
         }
     }
 }
--- Assets/Scripts/MovingCube.cs
+++ Assets/Scripts/MovingCube.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingCube : MonoBehaviour
@@ -8,18 +6,36 @@ public class MovingCube : MonoBehaviour
     [SerializeField] private float moveSpeed = 1f;
 
     public static MovingCube CurrentCube { get; private set; }
+    public static MovingCube LastCube { get; private set; }
 
     private void OnEnable()
     {
+        if (LastCube == null)
+            LastCube = GameObject.Find("Start").GetComponent<MovingCube>();
+
         CurrentCube = this;
     }
 
     void Update()
     {
-        transform.position += transform.forward * Time.deltaTime * moveSpeed;
+        transform.position -= transform.forward * Time.deltaTime * moveSpeed;
     }
     public void Stop()
     {
         moveSpeed = 0f;
+
+        float hangover = transform.position.z - LastCube.transform.position.z;
+        SplitCubeOnZ(hangover);
+    }
+
+    private void SplitCubeOnZ(float hangover)
+    {
+        float newZSize = LastCube.transform.localScale.z - Mathf.Abs(hangover);
+        float fallingCubeZSize = transform.localScale.z - newZSize;
+
+        float newZPosition = LastCube.transform.position.z + (hangover / 2);
+
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, newZSize);
+        transform.position = new Vector3(transform.position.x, transform.position.y, newZPosition);
     }
 }
